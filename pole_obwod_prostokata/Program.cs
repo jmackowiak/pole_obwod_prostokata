89e3ebcf97cb30cd0a696ad6868003e36a68095f
jmackowiak <Zalmitego977>
2019-10-17 17:39:37 +0200

    Add project files.

---
+++ pole_obwod_prostokata/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pole_obwod_prostokata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość pierwszego boku:");
+            string stra = Console.ReadLine();
+            Console.WriteLine("Podaj długość drugiego boku:");
+            string strb = Console.ReadLine();
+            Console.WriteLine();
+
+            if (float.TryParse(stra, out float boka) && float.TryParse(strb, out float bokb) == true)
+            {
+                float pole = boka * bokb;
+                float obwod = 2 * boka + 2 * bokb;
+
+                Console.WriteLine("Pole prostokąta wynosi: {0}", pole);
+                Console.WriteLine("Obwód prostokąta wynosi: {0}", obwod);
+            }
+            else
+            {
+                Console.WriteLine("Podałeś błędne dane!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
